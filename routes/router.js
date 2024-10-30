78be17a869a951754c4d65942cafb1d797ad3c84
Jovana <jovanastanicc@gmail.com>
2024-10-30 16:03:30 +0100

    cleaned up unused routes

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router()
-const authRouter = require("./authentification");
 const playRouter = require("./play");
 const adminRouter = require("./admin");
 const {registerUser, loginUser} = require('../controlers/authcontroler');
 const {getTop10Results} = require("../controlers/resultscontroler")
 
 
-router.use("/auth", authRouter);
+
 router.use("/play", playRouter);
 router.use("/admin", adminRouter);
 
